f8ac11e1be5e7cb553d348c4818cca920988a0d2
ObukhovVladislav <vobukhov10@gmail.com>
2020-12-15 21:37:59 +0500

    Adding your application to the settings file and adding data to url.py and views.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nmr#l&65!0ws2fk7ci+egtq2fwf=k#=ow*-m^9(v)gres6%@6&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
