ecbfe8a33110748d14e13e86d40de9ee2d29dc3c
Vladislav Obv <VladislavObv@example.com>
2021-01-24 18:05:05 +0500

    basketapp

--- project/basketapp/models.py
+++ project/basketapp/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from mainapp.models import Games
+
+
+class GamesBasket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    games = models.ForeignKey(Games, on_delete=models.CASCADE)
+    added = models.DateTimeField(auto_now_add=True)
--- project/basketapp/views.py
+++ project/basketapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from basketapp.models import GamesBasket
+
 
 def index(request):
     return render(request, 'basketapp/basket.html')
+
+
+def add(request, games_id):
+    basket_item = GamesBasket.objects.get_or_create(
+        user=request.user,
+        games=games_id
+    )
