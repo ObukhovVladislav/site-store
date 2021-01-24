e37be62d937962ef13a0a23d2992b84bc5ba7a48
Vladislav Obv <VladislavObv@example.com>
2021-01-24 13:23:52 +0500

    login page

--- project/authapp/views.py
+++ project/authapp/views.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.forms import AuthenticationForm
+# from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
+
+from authapp.forms import LoginForm
 
 
 def login(request):
-    form = AuthenticationForm()
+    # form = AuthenticationForm()
+    form = LoginForm
     context = {
         'page_title': 'Авторизация',
         'form': form,
