1be988aecc66bca8214c54d95c2d2369cb8eb919
Vladislav Obv <VladislavObv@example.com>
2021-01-24 13:30:35 +0500

    login page

--- project/authapp/forms.py
+++ project/authapp/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, item in self.fields.items():
+            item.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = User
         fields = ('username', 'password')
